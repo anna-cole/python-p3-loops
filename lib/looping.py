4f4863e9da67c7a4e1e497c439de490eaf9e88c1
Anna Raptidou Cole <acrrj123@gmail.com>
2023-12-20 11:22:31 -0600

    Lab done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+#happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [number * number for number in int_list]
+    return int_list
+#square_integers([1, 2, 3, 4, 5])
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100: 
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+
+#fizzbuzz()    
